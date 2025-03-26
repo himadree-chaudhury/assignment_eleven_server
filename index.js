d5a00329122d6dc2244e100c185eb8c0795d4a69
himadree-chaudhury <himchy2001@gmail.com>
2025-03-27 00:22:41 +0600

    install necessasry dependancies & configure mongodb

---
+++ index.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// label: Middleware
+app.use(cors());
+app.use(express.json());
+
+// label: MongoDB Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lhej2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+      );
+
+      //   label: Database Connection
+      const database = client.db(process.env.DB_NAME)
+
+
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to the DriveXpress server!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
